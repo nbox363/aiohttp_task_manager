e698fad74bb91637af629addc17926c9e37296b2
nbox363 <overchukdo@gmail.com>
2020-12-01 14:57:56 +0300

    feat: templates

--- app/task.py
+++ app/task.py
@@ -9,13 +9,36 @@ from app.db import get_db
 bp = Blueprint('task', __name__)
 
 
-@bp.route('/')
+def get_task(id, check_author=True):
+    task = get_db().execute(
+        'SELECT p.id, title, body, finish, author_id, email'
+        ' FROM task p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if task is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and task['author_id'] != g.user['id']:
+        abort(403)
+
+    return task
+
+
+@bp.route('/<int:id>', methods=['POST', 'GET'])
+def task_detail(id):
+    task = get_task(id)
+    return render_template('task/task_detail.html', task=task)
+
+
+@bp.route('/', methods=['POST', 'GET'])
 def index():
     db = get_db()
     tasks = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, finish, author_id, email'
         ' FROM task p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' ORDER BY finish DESC'
     ).fetchall()
     return render_template('task/index.html', tasks=tasks)
 
@@ -26,6 +49,7 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        finish = request.form['finish']
         error = None
 
         if not title:
@@ -36,31 +60,14 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO task (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO task (title, body, author_id, finish)'
+                ' VALUES (?, ?, ?, ?)',
+                (title, body, g.user['id'], finish)
             )
             db.commit()
             return redirect(url_for('task.index'))
 
     return render_template('task/create.html')
-
-
-def get_task(id, check_author=True):
-    task = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM task p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if task is None:
-        abort(404, "Task id {0} doesn't exist.".format(id))
-
-    if check_author and task['author_id'] != g.user['id']:
-        abort(403)
-
-    return task
 
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
@@ -91,7 +98,7 @@ def update(id):
     return render_template('task/update.html', task=task)
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+@bp.route('/<int:id>/delete', methods=('POST', 'GET'))
 @login_required
 def delete(id):
     get_task(id)
